1a82b1f551648ef328a94b76168a4dc1977b9930
Mike Chen <1325549+mhchen@users.noreply.github.com>
2022-12-05 21:13:35 -0800

    Day 6

---
+++ day6/2.ts
@@ -0,0 +1,22 @@
+import { parseInput } from '../utils';
+
+const input = parseInput()[0];
+
+const areCharsUnique = (str: string) => {
+  const chars = new Set();
+  for (const char of str) {
+    if (chars.has(char)) {
+      return false;
+    }
+    chars.add(char);
+  }
+  return true;
+};
+
+for (let i = 0, j = i + 14; j < input.length; i++, j++) {
+  const slice = input.slice(i, j);
+  if (areCharsUnique(slice)) {
+    console.log(j);
+    process.exit(0);
+  }
+}
